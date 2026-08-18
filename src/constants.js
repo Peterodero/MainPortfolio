c5d48f049afcf70231623e6e2e2dfc47f2191ce1
Peter Odero <135719866+Peterodero@users.noreply.github.com>
2026-08-18 09:50:41 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -136,7 +136,7 @@ export const education = [
   {
     id: 0,
     school: "Masinde Muliro University of Science and Technology",
-    date: "Dec 2022 - Dec 2026",
+    date: "Dec 2019 - Dec 2022",
     grade: "3.5 CGPA",
     desc: " Pursued Bachelor of Computer Science. This program has provided me with a robust foundation in software development principles and practices, including **Data Structures, Algorithms, Object-Oriented Programming, and Database Management Systems**. I am gaining practical experience in designing and developing software solutions while honing my skills in various programming languages and modern software development methodologies.",
     degree: "Bachelor of Computer Science",
@@ -144,7 +144,7 @@ export const education = [
   {
     id: 2,
     school: "Got Rateng' High School",
-    date: "March 2018 - July 2021",
+    date: "March 2014 - July 2018",
     grade: "A-",
     desc: "Completed my studies, achieving a strong academic foundation in various subjects. This period was crucial for developing my core knowledge and study habits.",
     degree: "Secondary Education",
@@ -152,7 +152,7 @@ export const education = [
   {
     id: 3,
     school: "Ayiengo Primary School",
-    date: "March 2007 - March 2017",
+    date: "March 2002 - March 2013",
     grade: "A-",
     desc: "Completed my primary school education, establishing a foundational understanding of core subjects. This period focused on building strong learning habits and preparing for higher academic levels",
     degree: "Primary Education",
